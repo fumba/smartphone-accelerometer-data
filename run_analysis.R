eef1a68633ef31ae714b60c51b9e5bfd662cecc2
fumbaa <fumba.me@gmail.com>
2016-03-28 20:52:07 -0400

    unzip datasource

--- run_analysis.R
+++ run_analysis.R
@@ -1,9 +1,11 @@
 #1.  Merge the training and test sets to create one dataset
+
 library(RCurl)
 
 if(!file.exists('./uci_smartphone_dataset/Dataset.zip')){
   dir.create('./uci_smartphone_dataset')
   dataUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
   download.file(dataUrl, destfile = "./uci_smartphone_dataset/Dataset.zip", method = "libcurl")
+  unzip('./uci_smartphone_dataset/Dataset.zip');
 }
 
